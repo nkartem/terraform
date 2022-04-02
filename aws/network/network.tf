e2fe2561318f1a394e3aea81beeba0f40c48062a
nkartem <boss_aaa@ukr.net>
2022-04-02 13:14:30 +0300

    main

---
+++ aws/network/network.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.58.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_eip" "lb" {
+  instance = aws_instance.web.id
+  vpc      = true
+}
