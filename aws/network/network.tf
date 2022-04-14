39796a88ef5f156a1ce807ca8098e3f1d65b131d
nkartem <boss_aaa@ukr.net>
2022-04-14 13:14:25 +0300

    Update network.tf

--- aws/network/network.tf
+++ aws/network/network.tf
@@ -12,82 +12,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-
-# Create Elastic IP
-resource "aws_eip" "eip1" {
-  vpc              = true
-}
-
-resource "aws_eip" "eip2" {
-}
-
-# Create security group
-resource "aws_security_group" "name_security_group1" {
-  name        = "SomeName1 Security Group"
-  description = "SecurityGroup ports 22 443 80 8080"
-# vpc_id      = aws_vpc.main.id
-
-dynamic "ingress" {
- for_each = ["22","443","8080", "80"]
-
-   content    {
-      description      = "open ports"
-      from_port        = ingress.value
-      to_port          = ingress.value
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-    }
-}
-
-  egress    {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-    }
-
-  tags = {
-    Name = "Security Group for SomeName1"
-  }
-}
-
-resource "aws_security_group" "name_security_group2" {
-  name        = "SomeName Security Group"
-  description = "SecurityGroup ports 22 21 3389"
-# vpc_id      = aws_vpc.main.id
-
-dynamic "ingress" {
- for_each = ["22","21"]
-
-   content    {
-      description      = "open ports"
-      from_port        = ingress.value
-      to_port          = ingress.value
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-   }
-}
-
-  ingress    {
-      description      = "open port RDP 3389"
-      from_port        = 3389
-      to_port          = 3389
-      protocol         = "tcp"
-      cidr_blocks      = ["79.81.32.25/32"]
-  }
-
-  egress    {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-    }
-
-  tags = {
-    Name = "Security Group for SomeName2"
-  }
-}
-
 # Create VPC
 resource "aws_vpc" "name_vpc" {
   cidr_block = "10.10.0.0/16"
@@ -95,21 +19,129 @@ resource "aws_vpc" "name_vpc" {
     Name = "name-vpc-terraform"
   }
 }
-# Create subnet
-resource "aws_subnet" "name_private_subnet" {
-  vpc_id     = aws_vpc.name_vpc.id
-  cidr_block = "10.10.1.0/24"
+
+# Internet Gateway
+resource "aws_internet_gateway" "name_igw" {
+  vpc_id = aws_vpc.name_vpc.id
 
   tags = {
-    Name = "Private"
+    Name = "Internet Gateway Terraform"
   }
 }
 
-resource "aws_subnet" "name_public_subnet" {
-  vpc_id     = aws_vpc.name_vpc.id
-  cidr_block = "10.10.2.0/24"
+# # Create subnet
+# resource "aws_subnet" "name_public_subnet" {
+#   vpc_id     = aws_vpc.name_vpc.id
+#   cidr_block = "10.10.1.0/24"
+
+#   tags = {
+#     Name = "Public"
+#   }
+# }
+
+# resource "aws_subnet" "name_private_subnet" {
+#   vpc_id     = aws_vpc.name_vpc.id
+#   cidr_block = "10.10.2.0/24"
+
+#   tags = {
+#     Name = "Private"
+#   }
+# }
+
+# resource "aws_subnet" "database" {
+#   vpc_id     = aws_vpc.name_vpc.id
+#   cidr_block = "10.10.3.0/24"
+
+#   tags = {
+#     Name = "database subnet"
+#   }
+# }
+
+resource "aws_route_table" "name_route_table" {
+  vpc_id = aws_vpc.name_vpc.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.name_igw.id
+  }
 
   tags = {
-    Name = "Public"
+    Name = "name_route_table"
   }
 }
+
+
+# # Create Elastic IP
+# # resource "aws_eip" "eip1" {
+# #   vpc              = true
+# # }
+
+# # resource "aws_eip" "eip2" {
+# # }
+
+# # Create security group
+# resource "aws_security_group" "name_security_group1" {
+#   name        = "SomeName1 Security Group"
+#   description = "SecurityGroup ports 22 443 80 8080"
+# # vpc_id      = aws_vpc.main.id
+
+# dynamic "ingress" {
+#  for_each = ["22","443","8080", "80"]
+
+#    content    {
+#       description      = "open ports"
+#       from_port        = ingress.value
+#       to_port          = ingress.value
+#       protocol         = "tcp"
+#       cidr_blocks      = ["0.0.0.0/0"]
+#     }
+# }
+
+#   egress    {
+#       from_port        = 0
+#       to_port          = 0
+#       protocol         = "-1"
+#       cidr_blocks      = ["0.0.0.0/0"]
+#     }
+
+#   tags = {
+#     Name = "Security Group for SomeName1"
+#   }
+# }
+
+# resource "aws_security_group" "name_security_group2" {
+#   name        = "SomeName Security Group"
+#   description = "SecurityGroup ports 22 21 3389"
+# # vpc_id      = aws_vpc.main.id
+
+# dynamic "ingress" {
+#  for_each = ["22","21"]
+
+#    content    {
+#       description      = "open ports"
+#       from_port        = ingress.value
+#       to_port          = ingress.value
+#       protocol         = "tcp"
+#       cidr_blocks      = ["0.0.0.0/0"]
+#    }
+# }
+
+#   ingress    {
+#       description      = "open port RDP 3389"
+#       from_port        = 3389
+#       to_port          = 3389
+#       protocol         = "tcp"
+#       cidr_blocks      = ["79.81.32.25/32"]
+#   }
+
+#   egress    {
+#       from_port        = 0
+#       to_port          = 0
+#       protocol         = "-1"
+#       cidr_blocks      = ["0.0.0.0/0"]
+#     }
+
+#   tags = {
+#     Name = "Security Group for SomeName2"
+#   }
+# }
