c6f4c0eda143fe7cc4526eded1bb33e602e0e2bd
nkartem <boss_aaa@ukr.net>
2022-04-15 10:21:40 +0300

    Add diagram
    Add diagram

--- aws/network/network.tf
+++ aws/network/network.tf
@@ -135,69 +135,102 @@ resource "aws_nat_gateway" "name_nat_gateway" {
   depends_on = [aws_internet_gateway.name_igw]
 }
 
-# # Create security group
-# resource "aws_security_group" "name_security_group1" {
-#   name        = "SomeName1 Security Group"
-#   description = "SecurityGroup ports 22 443 80 8080"
-# # vpc_id      = aws_vpc.main.id
+# Create security group
+resource "aws_security_group" "name_security_bastion" {
+  name        = "SomeName1 Security Group"
+  description = "SecurityGroup ports 22 3389"
+  vpc_id      = aws_vpc.name_vpc.id
 
-# dynamic "ingress" {
-#  for_each = ["22","443","8080", "80"]
+  ingress    {
+      description      = "open port ssh 22"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+  }
+  
+  ingress    {
+      description      = "open port RDP 3389"
+      from_port        = 3389
+      to_port          = 3389
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+  }
 
-#    content    {
-#       description      = "open ports"
-#       from_port        = ingress.value
-#       to_port          = ingress.value
-#       protocol         = "tcp"
-#       cidr_blocks      = ["0.0.0.0/0"]
-#     }
-# }
+  egress    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
 
-#   egress    {
-#       from_port        = 0
-#       to_port          = 0
-#       protocol         = "-1"
-#       cidr_blocks      = ["0.0.0.0/0"]
-#     }
+  tags = {
+    Name = "Security Group for Bastion server"
+  }
+}
 
-#   tags = {
-#     Name = "Security Group for SomeName1"
-#   }
-# }
+resource "aws_security_group" "name_security_group1" {
+  name        = "SomeName1 Security Group"
+  description = "SecurityGroup ports 22 443 80 8080"
+  vpc_id      = aws_vpc.name_vpc.id
 
-# resource "aws_security_group" "name_security_group2" {
-#   name        = "SomeName Security Group"
-#   description = "SecurityGroup ports 22 21 3389"
-# # vpc_id      = aws_vpc.main.id
+dynamic "ingress" {
+ for_each = ["22","443","8080", "80"]
 
-# dynamic "ingress" {
-#  for_each = ["22","21"]
+   content    {
+      description      = "open ports"
+      from_port        = ingress.value
+      to_port          = ingress.value
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+}
 
-#    content    {
-#       description      = "open ports"
-#       from_port        = ingress.value
-#       to_port          = ingress.value
-#       protocol         = "tcp"
-#       cidr_blocks      = ["0.0.0.0/0"]
-#    }
-# }
+  egress    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
 
-#   ingress    {
-#       description      = "open port RDP 3389"
-#       from_port        = 3389
-#       to_port          = 3389
-#       protocol         = "tcp"
-#       cidr_blocks      = ["79.81.32.25/32"]
-#   }
+  tags = {
+    Name = "Security Group for SomeName1"
+  }
+}
 
-#   egress    {
-#       from_port        = 0
-#       to_port          = 0
-#       protocol         = "-1"
-#       cidr_blocks      = ["0.0.0.0/0"]
-#     }
+resource "aws_security_group" "name_security_group2" {
+  name        = "SomeName Security Group"
+  description = "SecurityGroup ports 22 21 3389"
+  vpc_id      = aws_vpc.name_vpc.id
 
-#   tags = {
-#     Name = "Security Group for SomeName2"
-#   }
-# }
+dynamic "ingress" {
+ for_each = ["22","21"]
+
+   content    {
+      description      = "open ports"
+      from_port        = ingress.value
+      to_port          = ingress.value
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+   }
+}
+
+  ingress    {
+      description      = "open port RDP 3389"
+      from_port        = 3389
+      to_port          = 3389
+      protocol         = "tcp"
+      cidr_blocks      = ["79.81.32.25/32"]
+  }
+
+  egress    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+  tags = {
+    Name = "Security Group for SomeName2"
+  }
+}
