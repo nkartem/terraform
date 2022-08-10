a5cc80286aa06487046522727b0e128e63199586
Artem <boss_aaa@ukr.net>
2022-08-10 13:36:32 +0300

    Update aws-example-ec2.tf

--- aws/examle/aws-example-ec2.tf
+++ aws/examle/aws-example-ec2.tf
@@ -9,22 +9,51 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-#  region = "eu-central-1"
-#  region = "eu-north-1"
-  region = "eu-west-2"
+  region = "eu-central-1"
 }
 
 
 resource "aws_instance" "AL2" {
-#    ami = "ami-0c956e207f9d113d5" # eu-central-1
-#    ami = "ami-01977e30682e5df74" # eu-north-1
-    ami = "ami-0e34bbddc66def5ac" # eu-west-2
+    ami = "ami-0c956e207f9d113d5" # eu-central-1
     instance_type = "t2.micro"
+    user_data = file("install.sh")
+    vpc_security_group_ids = [ aws_security_group.web_al2.id ]
 #    count = 3 //the number of servers to create. To delete an instance, enter 0 or use the "terraform destroy" command.de
-#    key_name = "aml2" # You must create key
+    key_name = "terra" # You must create key
 
     tags = {
-      Name = "Amazon Linux 2"
+      Name = "AL2"
       OS = "Amazon Linux 2"
+      Project = "Git"
     }
+}
+
+resource "aws_security_group" "web_al2" {
+  name        = "WebServer Security Group"
+  description = "SecurityGroup ports 80 443 22"
+# vpc_id      = aws_vpc.main.id
+
+dynamic "ingress" {
+ for_each = ["80", "443", "22"]
+
+   content    {
+      description      = "open ports"
+      from_port        = ingress.value
+      to_port          = ingress.value
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+}
+
+  egress    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+  tags = {
+    Name = "Security Group WebServer"
+    WebServer = "nginx"
+  }
 }
