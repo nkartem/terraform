2536b7fe1c26e97ccd6fd8efef95adff7a3bb6e8
Artem <boss_aaa@ukr.net>
2022-08-18 11:49:08 +0300

    Update aws-example-ec2.tf

--- aws/examle/aws-example-ec2.tf
+++ aws/examle/aws-example-ec2.tf
@@ -33,17 +33,54 @@ resource "aws_security_group" "web_al2" {
   description = "SecurityGroup ports 80 443 22"
 # vpc_id      = aws_vpc.main.id
 
+
 dynamic "ingress" {
  for_each = ["80", "443", "22"]
 
    content    {
-      description      = "open ports"
+      description      = "ssh web"
       from_port        = ingress.value
       to_port          = ingress.value
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
     }
 }
+
+
+dynamic "ingress" {
+ for_each = ["1195", "2222"]
+
+   content    {
+      description      = "open ports"
+      from_port        = ingress.value
+      to_port          = ingress.value
+      protocol         = "udp"
+      cidr_blocks      = ["11.222.111.82/32", "14.214.141.61/32"]
+    }
+}
+
+dynamic "ingress" {
+ for_each = ["13222"]
+
+   content    {
+      description      = "open ports"
+      from_port        = ingress.value
+      to_port          = ingress.value
+      protocol         = "tcp"
+      cidr_blocks      = ["1.2.3.4/32", "4.3.2.1/32", "32.34.32.33/32"]
+    }
+}
+
+
+  ingress {
+      description      = "open ports for SIP"
+      from_port        = 10000
+      to_port          = 20000
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+}
+
+
 
   egress    {
       from_port        = 0
