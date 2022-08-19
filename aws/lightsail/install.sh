4dea94ff630c55d55a7fd8fe6794b760405f9409
Artem <boss_aaa@ukr.net>
2022-08-19 17:04:21 +0300

    Update install.sh

--- aws/lightsail/install.sh
+++ aws/lightsail/install.sh
@@ -2,13 +2,45 @@
 touch /home/bitnami/myfile.txt
 
 mkdir /opt/bitnami/projects
-#chown $USER /opt/bitnami/projects
+chown bitnami:bitnami /opt/bitnami/projects
 cd /opt/bitnami/projects
-express --view pug /opt/bitnami/projects/sample
-cd /opt/bitnami/projects/sample
-npm install
-DEBUG=sample:* ./bin/www
-forever start /opt/bitnami/projects/sample/bin/www
-cp /opt/bitnami/apache/conf/vhosts/sample-vhost.conf.disabled /opt/bitnami/apache/conf/vhosts/sample-vhost.conf
-cp /opt/bitnami/apache/conf/vhosts/sample-https-vhost.conf.disabled /opt/bitnami/apache/conf/vhosts/sample-https-vhost.conf
+express --view pug /opt/bitnami/projects/myapp
+cd /opt/bitnami/projects/myapp
+###############################
+touch /opt/bitnami/apache/conf/vhosts/myapp-http-vhost.conf
+cat > /opt/bitnami/apache/conf/vhosts/myapp-http-vhost.conf <<EOF
+  <VirtualHost _default_:80>
+    ServerAlias *
+    DocumentRoot "/opt/bitnami/projects/myapp/public"
+    <Directory "/opt/bitnami/projects/myapp/public">
+      Require all granted
+    </Directory>
+    ProxyPass / http://localhost:3000/
+    ProxyPassReverse / http://localhost:3000/
+  </VirtualHost>
+EOF
+
+touch /opt/bitnami/apache/conf/vhosts/myapp-https-vhost.conf
+cat > /opt/bitnami/apache/conf/vhosts/myapp-https-vhost.conf <<EOF
+  <VirtualHost _default_:443>
+    ServerAlias *
+    SSLEngine on
+    SSLCertificateFile "/opt/bitnami/apache/conf/bitnami/certs/server.crt"
+    SSLCertificateKeyFile "/opt/bitnami/apache/conf/bitnami/certs/server.key"
+    DocumentRoot "/opt/bitnami/projects/myapp"
+    <Directory "/opt/bitnami/projects/myapp">
+      Require all granted
+    </Directory>
+    ProxyPass / http://localhost:3000/
+    ProxyPassReverse / http://localhost:3000/
+  </VirtualHost>
+EOF
 /opt/bitnami/ctlscript.sh restart apache
+###########################
+
+npm install
+#DEBUG=myapp:* ./bin/www 
+forever start /opt/bitnami/projects/myapp/bin/www
+
+# cp /opt/bitnami/apache/conf/vhosts/sample-vhost.conf.disabled /opt/bitnami/apache/conf/vhosts/sample-vhost.conf
+# cp /opt/bitnami/apache/conf/vhosts/sample-https-vhost.conf.disabled /opt/bitnami/apache/conf/vhosts/sample-https-vhost.conf
