85e18cf62e9a402e40125ec7824e297983382f26
Artem <boss_aaa@ukr.net>
2022-08-30 17:49:25 +0300

    Create ec2s3.tf

---
+++ aws/ec2s3/ec2s3.tf
@@ -0,0 +1,70 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.58.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-central-1"
+}
+
+
+resource "aws_instance" "AL2" {
+    ami = "ami-0c956e207f9d113d5" # eu-central-1
+    instance_type = "t2.micro"
+    user_data = file("install.sh")
+    vpc_security_group_ids = [ aws_security_group.kafka_al2.id ]
+#    count = 3 //the number of servers to create. To delete an instance, enter 0 or use the "terraform destroy" command.de
+    key_name = "terra" # You must create key
+
+    tags = {
+      Name = "AL2"
+      OS = "Amazon Linux 2"
+      Project = "Kafka"
+    }
+}
+
+resource "aws_security_group" "kafka_al2" {
+  name        = "WebServer Security Group"
+  description = "SecurityGroup ports 80 443 22"
+# vpc_id      = aws_vpc.main.id
+
+
+dynamic "ingress" {
+ for_each = ["8080", "22", "9092", "9093"]
+
+   content    {
+      description      = "ssh web"
+      from_port        = ingress.value
+      to_port          = ingress.value
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+}
+
+  egress    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+  tags = {
+    Name = "Security Group WebServer"
+    MQ = "Kafka"
+  }
+}
+
+
+resource "aws_s3_bucket" "buck" {
+  bucket = "my-tf-test-bucketmq-kafka"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
