dc23e15ef85cf69a7010fbbaa6ed52b8dd65adb7
Artem <boss_aaa@ukr.net>
2022-11-07 18:26:07 +0200

    Update lambda.py

--- aws/asg/python/lambda.py
+++ aws/asg/python/lambda.py
@@ -1,13 +1,6 @@
 import json
 
 def lambda_handler(event, context):
-    message = "Server".format(event['key1'])
+    message = "Server".format(event['key1','key2'])
     print(message)
     return message
-    
-    
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
