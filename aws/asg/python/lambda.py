884f25be02e02067bc99cdbfd08cd658089ed0a6
Artem <boss_aaa@ukr.net>
2022-11-08 15:01:34 +0200

    Update lambda.py

--- aws/asg/python/lambda.py
+++ aws/asg/python/lambda.py
@@ -1,6 +1,13 @@
 import json
 
 def lambda_handler(event, context):
-    message = "Server".format(event['key1','key2'])
-    print(message)
-    return message
+    if format(event['key1']) == "add":
+        message = "Add Server on ASG {}".format(event['key1'])
+        print(message)
+        return message
+    elif format(event['key2']) == "remove":
+        message = "Remove Server on ASG {}".format(event['key2'])
+        print(message)
+        return message
+    else:
+        print ("Error, can not find key!")
