e5374fe8d390eb4d762c193dcbc6d095b801ea84
Artem <boss_aaa@ukr.net>
2022-11-18 16:38:11 +0200

    Create sender.py

---
+++ aws/asg/lambda-src/sender.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+x = 11
+
+if x > 10:
+    url_add = "https://q5z53tz230.execute-api.us-west-2.amazonaws.com/add-stage/add"
+    url = url_add
+    key1 = {"key1":"add"}
+    result = requests.post(url, data=json.dumps(key1))
+    print(result)
+    print(result.text)
+    print(result.ok)
+
+elif x <= 10:
+    url_remove = "https://q5z53tz230.execute-api.us-west-2.amazonaws.com/remove-stage/addremove"
+    url = url_remove
+    key2 = {"key2":"remove"}
+    result = requests.post(url, data=json.dumps(key2))
+    print(result)
+    print(result.text)
+    print(result.ok)
+
+else:
+    print("nothing doing")
